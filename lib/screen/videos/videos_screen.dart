e0a630349b6afa18867b57784321ffa20cb58060
soufian200 <soufianxlm@gmail.com>
2024-07-26 12:00:11 +0100

    remove firestore

--- lib/screen/videos/videos_screen.dart
+++ lib/screen/videos/videos_screen.dart
@@ -17,62 +17,6 @@ import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
 /// Stateful widget to fetch and then display video content.
-class VideoApp extends StatefulWidget {
-  final String url;
-  const VideoApp({super.key, required this.url});
-
-  @override
-  _VideoAppState createState() => _VideoAppState();
-}
-
-class _VideoAppState extends State<VideoApp> {
-  late VideoPlayerController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = VideoPlayerController.networkUrl(Uri.parse(widget.url))
-      ..initialize().then((_) {
-        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
-        setState(() {});
-      });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Video Demo',
-      home: Scaffold(
-        body: Center(
-          child: _controller.value.isInitialized
-              ? AspectRatio(
-                  aspectRatio: _controller.value.aspectRatio,
-                  child: VideoPlayer(_controller),
-                )
-              : Container(),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              _controller.value.isPlaying
-                  ? _controller.pause()
-                  : _controller.play();
-            });
-          },
-          child: Icon(
-            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-          ),
-        ),
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-}
 
 class VideosScreen extends StatefulWidget {
   const VideosScreen({super.key});
@@ -89,167 +33,114 @@ class _VideosScreenState extends State<VideosScreen> {
 
   @override
   Widget build(BuildContext context) {
+    List<VideoModel> videos = _firestoreService.getVideos();
     return SafeArea(
       child: Scaffold(
-        key: scaffoldKey,
-        backgroundColor: Colors.black,
-        appBar: PreferredSize(
-          preferredSize: Size(MediaQuery.of(context).size.width, 60.h),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 0, 174, 113),
-            ),
-            height: 30.h,
-            width: double.infinity,
-            alignment: Alignment.center,
-            child: Text(
-              "This Month Videos",
-              style: MyStyle.dashTextStyle.copyWith(
-                fontSize: ScreenUtil().setSp(18),
-                fontFamily: "arial",
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
+          key: scaffoldKey,
+          backgroundColor: Colors.black,
+          appBar: PreferredSize(
+            preferredSize: Size(MediaQuery.of(context).size.width, 60.h),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 0, 174, 113),
+              ),
+              height: 30.h,
+              width: double.infinity,
+              alignment: Alignment.center,
+              child: Text(
+                "This Month Videos",
+                style: MyStyle.dashTextStyle.copyWith(
+                  fontSize: ScreenUtil().setSp(18),
+                  fontFamily: "arial",
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ),
-        ),
-        body: StreamBuilder<List<VideoModel>>(
-          stream: _firestoreService.getVideos(),
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              print("#############|ERROR->|##");
-              return Center(child: Text('Error: ${snapshot.error}'));
-            }
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            }
+          body: Container(
+            child: Stack(
+              children: [
+                SizedBox(height: 12.h),
+                //We need swiper for every content
 
-            final videos = snapshot.data ?? [];
-            // print("======> data");
-            // print(videos.first.name);
-            // print(videos.first.url);
+                Swiper(
+                  itemBuilder: (BuildContext context, int index) {
+                    return videos.isEmpty
+                        ? CircularProgressIndicator()
+                        // : VideoApp(url: videos.first.url!);
+                        : ContentScreen(
+                            src: videos[index].url!,
+                            slide: videos[index].name,
+                          );
+                  },
+                  itemCount: videos.length,
+                  scrollDirection: Axis.vertical,
+                ),
+              ],
+            ),
+          )
+          // ----------- for firebase store --------------
+          // body: StreamBuilder<List<VideoModel>>(
+          //   stream: _firestoreService.getVideos(),
+          //   builder: (context, snapshot) {
+          //     if (snapshot.hasError) {
+          //       print("#############|ERROR->|##");
+          //       return Center(child: Text('Error: ${snapshot.error}'));
+          //     }
+          //     if (snapshot.connectionState == ConnectionState.waiting) {
+          //       return Center(child: CircularProgressIndicator());
+          //     }
 
-            return Container(
-              child: Stack(
-                children: [
-                  SizedBox(height: 12.h),
-                  //We need swiper for every content
+          //     final videos = snapshot.data ?? [];
 
-                  Swiper(
-                    itemBuilder: (BuildContext context, int index) {
-                      return videos.isEmpty
-                          ? CircularProgressIndicator()
-                          // : VideoApp(url: videos.first.url!);
-                          : ContentScreen(
-                              src: videos[index].url!,
-                              slide: videos[index].name,
-                            );
-                    },
-                    itemCount: videos.length,
-                    scrollDirection: Axis.vertical,
-                  ),
-                ],
-              ),
-            );
-          },
-        ),
+          //     return Container(
+          //       child: Stack(
+          //         children: [
+          //           SizedBox(height: 12.h),
+          //           //We need swiper for every content
 
-        // Padding(
-        //   padding: EdgeInsets.symmetric(horizontal: 16),
-        //   child: Text(
-        //     "المغرب",
-        //     style: MyStyle.dash13TextStyle,
-        //   ),
-        // ),
-        // VideoDetails(),
-        // ListView.builder(
-        //   physics: NeverScrollableScrollPhysics(),
-        //   shrinkWrap: true,
-        //   primary: true,
-        //   itemCount: videos!.length,
-        //   itemBuilder: (BuildContext context, int index) {
-        //     return OneVid(videos![index]);
-        //   },
-        // ),
-        //SizedBox(height: 16),
-        //HomePage()
+          //           Swiper(
+          //             itemBuilder: (BuildContext context, int index) {
+          //               return videos.isEmpty
+          //                   ? CircularProgressIndicator()
+          //                   // : VideoApp(url: videos.first.url!);
+          //                   : ContentScreen(
+          //                       src: videos[index].url!,
+          //                       slide: videos[index].name,
+          //                     );
+          //             },
+          //             itemCount: videos.length,
+          //             scrollDirection: Axis.vertical,
+          //           ),
+          //         ],
+          //       ),
+          //     );
+          //   },
+          // ),
 
-        // ),
-      ),
+          // Padding(
+          //   padding: EdgeInsets.symmetric(horizontal: 16),
+          //   child: Text(
+          //     "المغرب",
+          //     style: MyStyle.dash13TextStyle,
+          //   ),
+          // ),
+          // VideoDetails(),
+          // ListView.builder(
+          //   physics: NeverScrollableScrollPhysics(),
+          //   shrinkWrap: true,
+          //   primary: true,
+          //   itemCount: videos!.length,
+          //   itemBuilder: (BuildContext context, int index) {
+          //     return OneVid(videos![index]);
+          //   },
+          // ),
+          //SizedBox(height: 16),
+          //HomePage()
+
+          // ),
+          ),
     );
   }
-
-  List<VideoModel>? videoss = [
-    VideoModel(
-      id: 1,
-      name: " I Can't Believe This Happened To Me in Akchour ",
-      url:
-          'https://firebasestorage.googleapis.com/v0/b/gooto-80b74.appspot.com/o/Dakhla.mp4?alt=media&token=fb666e29-5824-4d8d-84c8-b5f978236d75',
-    ),
-    // VideoModel(
-    //   id: 1,
-    //   name: " MOROCCO TRAVEL DOCUMENTARY | The Grand Moroccan Roadtrip ",
-    //   url: "assets/video/travelmoroco.mp4",
-    // ),
-    // VideoModel(
-    //   id: 3,
-    //   name:
-    //       "The King of Moroccan Tagine Food 🇲🇦 Crazy Casablanca Food Tour!! ",
-    //   url: "assets/video/tagine.mp4",
-    // ),
-    // VideoModel(
-    //   id: 4,
-    //   name: " We are off to a New Continent! (Ferry from Spain to Morocco) ",
-    //   url: "assets/video/document.mp4",
-    // ),
-    // VideoModel(
-    //   id: 5,
-    //   name:
-    //       "10 Beautiful Places to Visit in Morocco 🇲🇦 | Must See Morocco Travel Guide ",
-    //   url: "assets/video/10days.mp4",
-    // ),
-    // VideoModel(
-    //   id: 5,
-    //   name:
-    //       "Beautiful Places to Visit in Dakhla 🇲🇦 | Must See Morocco Travel Guide ",
-    //   url: "assets/video/Dakhla.mp4",
-    // ),
-    // //   VideoModel(
-    // //   id: 5,
-
-    // //   name: "Beautiful Tagine in Morocco 🇲🇦 | Must See Morocco Travel Guide ",
-
-    // //   url: "assets/video/agine.mp4",
-    // // ),
-    // VideoModel(
-    //   id: 5,
-    //   name:
-    //       "Beautiful Places to Visit in casablanca 🇲🇦 | Must See Morocco Travel Guide ",
-    //   url: "assets/video/casablanca.mp4",
-    // ),
-    // VideoModel(
-    //   id: 5,
-    //   name:
-    //       "Beautiful Places to Visit in Marrakech 🇲🇦 | Must See Morocco Travel Guide ",
-    //   url: "assets/video/Marrakech.mp4",
-    // ),
-    // VideoModel(
-    //   id: 5,
-    //   name:
-    //       "Beautiful Places to Visit in Marzouga 🇲🇦 | Must See Morocco Travel Guide ",
-    //   url: "assets/video/Marzouga.mp4",
-    // ),
-    // VideoModel(
-    //   id: 5,
-    //   name:
-    //       " Beautiful Places to Visit in Morocco 🇲🇦 | Must See Morocco Travel Guide ",
-    //   url: "assets/video/Lharba.mp4",
-    // ),
-    // VideoModel(
-    //   id: 5,
-    //   name:
-    //       "Beautiful Places to Visit in Essaouira 🇲🇦 | Must See Morocco Travel Guide ",
-    //   url: "assets/video/Essaouira.mp4",
-    // ),
-  ];
 }
--- lib/services/service/firestore_service.dart
+++ lib/services/service/firestore_service.dart
@@ -17,8 +17,94 @@ class FirestoreService {
     return batch.commit();
   }
 
-  Stream<List<VideoModel>> getVideos() {
-    return _db.collection('videos').snapshots().map((snapshot) =>
-        snapshot.docs.map((doc) => VideoModel.fromFirestore(doc)).toList());
+  // Stream<List<VideoModel>> getVideos() {
+  // return _db.collection('videos').snapshots().map((snapshot) =>
+  //     snapshot.docs.map((doc) => VideoModel.fromFirestore(doc)).toList());
+  // }
+  List<VideoModel> getVideos() {
+    List<VideoModel> videoss = [
+      VideoModel(
+        id: 1,
+        name: "I Can't Believe This Happened To Me in Akchour",
+        url:
+            'https://firebasestorage.googleapis.com/v0/b/gooto-80b74.appspot.com/o/akchoure.mp4?alt=media&token=cfb88fb8-c018-49ba-a410-8e6310ef6ef9',
+      ),
+      VideoModel(
+        id: 2,
+        name: " MOROCCO TRAVEL DOCUMENTARY | The Grand Moroccan Roadtrip ",
+        url:
+            "https://firebasestorage.googleapis.com/v0/b/gooto-80b74.appspot.com/o/travelmoroco.mp4?alt=media&token=e3cf125d-1b7d-4232-a685-da1ca9ea0dc8",
+      ),
+      VideoModel(
+        id: 3,
+        name:
+            "The King of Moroccan Tagine Food 🇲🇦 Crazy Casablanca Food Tour!!",
+        url:
+            "https://firebasestorage.googleapis.com/v0/b/gooto-80b74.appspot.com/o/tagine.mp4?alt=media&token=7d9c96de-98cc-40a6-b8ba-bcdfd57dd8b4",
+      ),
+      VideoModel(
+        id: 4,
+        name: " We are off to a New Continent! (Ferry from Spain to Morocco) ",
+        url:
+            "https://firebasestorage.googleapis.com/v0/b/gooto-80b74.appspot.com/o/document.mp4?alt=media&token=05f2c549-9ba9-406f-8b84-aea6f434ed6c",
+      ),
+      VideoModel(
+        id: 5,
+        name:
+            "10 Beautiful Places to Visit in Morocco 🇲🇦 | Must See Morocco Travel Guide ",
+        url:
+            "https://firebasestorage.googleapis.com/v0/b/gooto-80b74.appspot.com/o/10days.mp4?alt=media&token=330f86a1-08be-4464-8118-f5460ebd5ed0",
+      ),
+      VideoModel(
+        id: 5,
+        name:
+            "Beautiful Places to Visit in Dakhla 🇲🇦 | Must See Morocco Travel Guide ",
+        url:
+            "https://firebasestorage.googleapis.com/v0/b/gooto-80b74.appspot.com/o/Dakhla.mp4?alt=media&token=fb666e29-5824-4d8d-84c8-b5f978236d75",
+      ),
+      VideoModel(
+        id: 6,
+        name:
+            "Beautiful Tagine in Morocco 🇲🇦 | Must See Morocco Travel Guide ",
+        url:
+            "https://firebasestorage.googleapis.com/v0/b/gooto-80b74.appspot.com/o/tagine.mp4?alt=media&token=7d9c96de-98cc-40a6-b8ba-bcdfd57dd8b4",
+      ),
+      VideoModel(
+        id: 7,
+        name:
+            "Beautiful Places to Visit in casablanca 🇲🇦 | Must See Morocco Travel Guide ",
+        url:
+            "https://firebasestorage.googleapis.com/v0/b/gooto-80b74.appspot.com/o/casablanca.mp4?alt=media&token=cc75894f-4258-4f44-bd23-1c4bd8a7e34d",
+      ),
+      VideoModel(
+        id: 8,
+        name:
+            "Beautiful Places to Visit in Marrakech 🇲🇦 | Must See Morocco Travel Guide ",
+        url:
+            "https://firebasestorage.googleapis.com/v0/b/gooto-80b74.appspot.com/o/Marrakech.mp4?alt=media&token=e16ae312-3077-499f-94dd-90ae3248d98e",
+      ),
+      VideoModel(
+        id: 9,
+        name:
+            "Beautiful Places to Visit in Marzouga 🇲🇦 | Must See Morocco Travel Guide ",
+        url:
+            "https://firebasestorage.googleapis.com/v0/b/gooto-80b74.appspot.com/o/Marzouga.mp4?alt=media&token=7773cbfa-7a1b-4def-8292-01280cc0d12e",
+      ),
+      VideoModel(
+        id: 10,
+        name:
+            " Beautiful Places to Visit in Morocco 🇲🇦 | Must See Morocco Travel Guide ",
+        url:
+            "https://firebasestorage.googleapis.com/v0/b/gooto-80b74.appspot.com/o/Lharba.mp4?alt=media&token=1954f5a3-d850-4b25-b329-ab2ac4cb85a5",
+      ),
+      VideoModel(
+        id: 11,
+        name:
+            "Beautiful Places to Visit in Essaouira 🇲🇦 | Must See Morocco Travel Guide ",
+        url:
+            "https://firebasestorage.googleapis.com/v0/b/gooto-80b74.appspot.com/o/Essaouira.mp4?alt=media&token=6ee6f3fa-b66d-4788-ab75-3f3d22e7d172",
+      ),
+    ];
+    return videoss;
   }
 }
