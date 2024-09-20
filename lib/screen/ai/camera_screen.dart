30cbc8d1ee99194b602a6deab09e774e896dbbb9
mastermajidosse <mastermajidosse@gmail.com>
2024-09-20 12:10:47 +0100

    modify ai

--- lib/screen/ai/camera_screen.dart
+++ lib/screen/ai/camera_screen.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:gooto/main_dev.dart';
-import 'package:gooto/screen/auth/splash_screen.dart';
 import 'package:markdown_widget/widget/all.dart';
 
 class CameraScreen extends StatefulWidget {
@@ -30,12 +29,39 @@ class _CameraScreenState extends State<CameraScreen> {
   void initState() {
     super.initState();
 
+    // }
+//    void _showModal() {
+//  showModalBottomSheet(
+//       context: context,
+//       isScrollControlled: true,
+//       shape: const RoundedRectangleBorder(
+//         borderRadius: BorderRadius.vertical(
+//           top: Radius.circular(20),
+//         ),
+//       ),
+//       builder: (BuildContext context) {
+//         return Padding(
+//           padding: MediaQuery.of(context).viewInsets,
+//           child: Container(
+//             padding: const EdgeInsets.all(20),
+//             child: MarkdownWidget(data: res),
+//           ),
+//         );
+//       },
+//     );
+//   }
+//      void _handleActionChange() {
+//     if (_actionNotifier.value == false) {
+//       _showModal();
+//     }
+
     _controller = CameraController(
       cameras[0],
       ResolutionPreset.high,
     );
     _initializeControllerFuture = _controller.initialize();
 
+    // loadit();
     gobro();
   }
 
@@ -48,9 +74,14 @@ class _CameraScreenState extends State<CameraScreen> {
   }
 
   loadit() async {
+    // const prompt = 'describe this picture';
+    // final prompt = TextPart("Describe this picture");
     final prompt = TextPart(
         "Describe this monument historique from moroco tell all info builder and Year of construction and  inside this picture ");
-
+    //in your answer just give me 500 character
+    // final content = [Content.text(prompt)];
+    // final response = await model.generateContent(content);
+    // try {
     try {
       await _initializeControllerFuture;
 
@@ -65,6 +96,8 @@ class _CameraScreenState extends State<CameraScreen> {
           Content.multi([prompt, ...imageParts])
         ]);
 
+        print("----------> response");
+        print(response.text);
         setState(() {
           res = response.text ?? "";
         });
@@ -77,7 +110,24 @@ class _CameraScreenState extends State<CameraScreen> {
         // Handle other exceptions
         print('Exception: $e');
       }
+    } finally {
+      // Dispose the CameraController when done
+      // await _controller?.dispose();
     }
+    // await _initializeControllerFuture;
+    // final image = await _controller.takePicture();
+
+    // final imageParts = [
+    //   DataPart('image/jpeg', await image.readAsBytes()),
+    // ];
+    // final response = await model.generateContent([
+    //   Content.multi([prompt, ...imageParts])
+    // ]);
+    // print("----------> response");
+    // print(response.text);
+    // setState(() {
+    //   res = response.text ?? "";
+    // });
   }
 
   @override
@@ -90,8 +140,13 @@ class _CameraScreenState extends State<CameraScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Monument Historique'),
-        automaticallyImplyLeading: false,
+        title: const Text('Monument Historique'), automaticallyImplyLeading: false,
+        // leading: IconButton(
+        //     icon: Icon(Icons.arrow_back),
+        //     onPressed: () {
+        //       Navigator.of(context).pop();
+        //     },
+        //   ),
       ),
       body: Stack(
         children: [
@@ -110,9 +165,8 @@ class _CameraScreenState extends State<CameraScreen> {
               : Container(
                   width: 300,
                   padding: const EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20)),
+                  decoration:
+                      BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(20)),
                   child: GestureDetector(
                     onTap: () {
                       showModalBottomSheet(
@@ -141,9 +195,41 @@ class _CameraScreenState extends State<CameraScreen> {
                       'Show description for this Monument ',
                       style: TextStyle(color: Colors.black),
                     ),
+                    // Container(
+                    //   child: Text(
+                    //     'Markdown Content',
+                    //     style: const TextStyle(color: Colors.black),
+                    //   ),
+                    //   //MarkdownWidget(data: res)
+                    // )
                   ))
+          //   ValueListenableBuilder<bool>(
+          //   valueListenable: _actionNotifier,
+          //   builder: (context, value, child) {
+          //     return
+          //     Switch(
+          //       value: value,
+          //       onChanged: (newValue) {
+          //         _actionNotifier.value = newValue;
+          //       },
+          //     );
+          //   },
+          // ),
         ],
       ),
+      // floatingActionButton: FloatingActionButton(
+      //   child: const Icon(Icons.camera_alt),
+      //   onPressed: () async {
+      //     // try {
+      //     //   await _initializeControllerFuture;
+      //     //   final image = await _controller.takePicture();
+      //     //   // Handle the captured image
+      //     // } catch (e) {
+      //     //   print(e);
+      //     // }
+      //     loadit();
+      //   },
+      // ),
     );
   }
 }
